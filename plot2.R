b8cf92ac47c4ec027c71b41b88ed09e53211e94e
nayda-b <nayda.barrios@gmail.com>
2015-04-12 22:58:39 -0500

    Plot 2 Creation

--- plot2.R
+++ plot2.R
@@ -5,6 +5,6 @@ dataset$Date<-as.Date(dataset$Date,"%d/%m/%Y")
 #Subsetting the dataset for only 2 dates
 sdataset<-subset(dataset, Date>="2007-02-01" & Date<="2007-02-02")
 #Creating the plot
-png("plot1.png")
-hist(sdataset$Global_active_power, xlab="Global Active Power (kilowatts)", ylab="Frecuency", main="Global Active Power", col = "red")
+png("plot2.png")
+plot(sdataset$Global_active_power, type="l")
 dev.off()
