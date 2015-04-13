ea949a6ab2659a5d61edba658035842c69606ca9
nayda-b <nayda.barrios@gmail.com>
2015-04-12 23:00:43 -0500

    Plot 1 Creation

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 dataset=read.table("~/household_power_consumption.txt",na.strings="?",header=TRUE, sep=";")
 #Convert the Date to R Date class
 dataset$Date<-as.Date(dataset$Date,"%d/%m/%Y")
-#Subsetting the dataset for only 2 dates
+#Subsetting the dataset
 sdataset<-subset(dataset, Date>="2007-02-01" & Date<="2007-02-02")
 #Creating the plot
 png("plot1.png")
